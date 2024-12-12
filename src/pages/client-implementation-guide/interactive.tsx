7f07124fbcb3532377b92fa89d01a3aec9b97d45
Quentin Gliech <quenting@element.io>
2024-12-12 10:09:52 +0100

    Add a device code grant guide

--- src/pages/client-implementation-guide/interactive.tsx
+++ src/pages/client-implementation-guide/interactive.tsx
@@ -1,15 +1,17 @@
 import {
   SpinnerIcon,
   RestartIcon,
+  CopyIcon,
 } from "@vector-im/compound-design-tokens/assets/web/icons";
 import { useStore } from "@nanostores/react";
-import { Alert, Button, Form } from "@vector-im/compound-web";
+import { Alert, Button, Form, Link } from "@vector-im/compound-web";
 import { atom, computed, task, type WritableAtom } from "nanostores";
 import { persistentAtom } from "@nanostores/persistent";
 import type React from "react";
 import { useEffect, useState } from "react";
 import cx from "classnames";
 import { QueryClient, useMutation, useQuery } from "@tanstack/react-query";
+import { QRCodeSVG } from "qrcode.react";
 
 import styles from "./style.module.css";
 
@@ -36,6 +38,8 @@ const serverMetadata = persistentAtom<ServerMetadata>(
     authorization_endpoint: "https://auth-oidc.element.dev/authorize",
     token_endpoint: "https://auth-oidc.element.dev/oauth2/token",
     registration_endpoint: "https://auth-oidc.element.dev/oauth2/registration",
+    device_authorization_endpoint:
+      "https://auth-oidc.element.dev/oauth2/device",
   },
   { encode: JSON.stringify, decode: JSON.parse },
 );
@@ -56,6 +60,12 @@ const accessToken = persistentAtom<string | null>("access-token", null, {
   decode: JSON.parse,
 });
 const refreshToken = persistentAtom("refresh-token");
+const deviceCode = persistentAtom("device-code");
+const userCode = persistentAtom("user-code");
+const verificationUri = persistentAtom(
+  "verification-uri",
+  "https://auth-oidc.element.dev/link",
+);
 
 const computeCodeChallenge = async (codeVerifier: string): Promise<string> => {
   // Hash the verifier
@@ -289,6 +299,7 @@ type ServerMetadata = {
   authorization_endpoint: string;
   token_endpoint: string;
   registration_endpoint: string;
+  device_authorization_endpoint: string;
 };
 
 export const OidcMetadataFetcher = () => {
@@ -322,7 +333,9 @@ const DataViewer: React.FC<{ data: unknown }> = ({
   );
 };
 
-export const ClientMetadataForm = () => {
+export const ClientMetadataForm: React.FC<{ grantTypes: string[] }> = ({
+  grantTypes,
+}) => {
   const $serverMetadata = useStore(serverMetadata);
   const [clientName, setClientName] = useState("Client implementation guide");
   const [response, setResponse] = useState<null | object>(null);
@@ -352,19 +365,24 @@ export const ClientMetadataForm = () => {
     client,
   );
 
+  const hasAuthorizationCodeGrant = grantTypes.includes("authorization_code");
+
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setClientName(e.target.value);
   };
 
-  const clientMetadata = {
+  const clientMetadata: Record<string, unknown> = {
     application_type: "web",
     client_name: clientName,
     client_uri: clientUri,
     token_endpoint_auth_method: "none",
-    redirect_uris: [redirectUri],
-    response_types: ["code"],
-    grant_types: ["authorization_code", "refresh_token"],
+    grant_types: grantTypes,
   };
+
+  if (hasAuthorizationCodeGrant) {
+    clientMetadata.redirect_uris = [redirectUri];
+    clientMetadata.response_types = ["code"];
+  }
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -405,6 +423,144 @@ export const ClientMetadataForm = () => {
 
         {response && <DataViewer data={response} />}
       </Form.Root>
+    </div>
+  );
+};
+
+export const DeviceCodeRequestForm = () => {
+  const $clientId = useStore(clientId) || "";
+  const $serverMetadata = useStore(serverMetadata);
+  const $queryClient = useStore(queryClient);
+
+  const [response, setResponse] = useState<null | object>(null);
+
+  const mutation = useMutation(
+    {
+      mutationFn: async (): Promise<void> => {
+        const params = {
+          client_id: $clientId,
+          scope:
+            "urn:matrix:org.matrix.msc2967.client:api:* urn:matrix:org.matrix.msc2967.client:device:ABCDEFGHIJKL",
+        } satisfies Record<string, string>;
+
+        const body = new URLSearchParams(params).toString();
+
+        const res = await fetch($serverMetadata.device_authorization_endpoint, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Accept: "application/json",
+          },
+          body,
+        });
+
+        const data = await res.json();
+        setResponse(data);
+
+        if ("device_code" in data && typeof data.device_code === "string") {
+          deviceCode.set(data.device_code);
+        }
+
+        if ("user_code" in data && typeof data.user_code === "string") {
+          userCode.set(data.user_code);
+        }
+
+        if (
+          "verification_uri" in data &&
+          typeof data.verification_uri === "string"
+        ) {
+          verificationUri.set(data.verification_uri);
+        }
+      },
+    },
+    $queryClient,
+  );
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    mutation.mutate();
+  };
+
+  return (
+    <div className={cx(styles["form-wrapper"])}>
+      <Form.Root className={cx(styles.form)} onSubmit={onSubmit}>
+        <Form.Field name="endpoint">
+          <Form.Label>Device authorization endpoint</Form.Label>
+          <Form.TextControl
+            type="url"
+            readOnly
+            value={$serverMetadata.device_authorization_endpoint}
+          />
+        </Form.Field>
+        <Form.Field name="client-id">
+          <Form.Label>Client ID</Form.Label>
+          <Form.TextControl type="text" required readOnly value={$clientId} />
+          {!$clientId && (
+            <Form.ErrorMessage>
+              Client must be registered to get one
+            </Form.ErrorMessage>
+          )}
+        </Form.Field>
+
+        <Form.Submit size="sm" kind="secondary" disabled={!$clientId}>
+          Start device authorization flow
+        </Form.Submit>
+
+        {response && <DataViewer data={response} />}
+      </Form.Root>
+    </div>
+  );
+};
+
+export const DeviceCodeQrCode = () => {
+  const $queryClient = useStore(queryClient);
+  const $verificationUri = useStore(verificationUri);
+  const $userCode = useStore(userCode);
+
+  const mutation = useMutation(
+    {
+      mutationFn: async (code: string): Promise<void> => {
+        await navigator.clipboard.writeText(code);
+      },
+      onSettled: () => {
+        setTimeout(() => {
+          mutation.reset();
+        }, 2000);
+      },
+    },
+    $queryClient,
+  );
+
+  return (
+    <div className={cx(styles["qr-code"])}>
+      <QRCodeSVG value={$verificationUri} />
+      <Link target="_blank" href={$verificationUri}>
+        {$verificationUri}
+      </Link>
+      {$userCode ? (
+        <>
+          <Form.MFAInput
+            className={cx(styles.code)}
+            readOnly
+            value={$userCode}
+          />
+          <Button
+            Icon={CopyIcon}
+            kind="secondary"
+            size="sm"
+            onClick={(e) => {
+              e.preventDefault();
+              if ($userCode === "") return;
+              mutation.mutate($userCode);
+            }}
+            disabled={$userCode === "" || mutation.status !== "idle"}
+          >
+            {mutation.status === "success" ? "Copied!" : "Copy code"}
+          </Button>
+        </>
+      ) : (
+        "Start the device authorisation flow to get a code"
+      )}
     </div>
   );
 };
@@ -511,6 +667,100 @@ export const CodeExchangeForm = () => {
 
         <Form.Submit size="sm" kind="secondary" disabled={!$clientId}>
           Exchange code for access token
+        </Form.Submit>
+
+        {response && <DataViewer data={response} />}
+      </Form.Root>
+    </div>
+  );
+};
+
+export const DeviceCodeExchangeForm = () => {
+  const $clientId = useStore(clientId) || "";
+  const $serverMetadata = useStore(serverMetadata);
+  const $deviceCode = useStore(deviceCode) || "";
+  const $queryClient = useStore(queryClient);
+
+  const [response, setResponse] = useState<null | object>(null);
+
+  const mutation = useMutation(
+    {
+      mutationFn: async (): Promise<void> => {
+        const params = {
+          grant_type: "urn:ietf:params:oauth:grant-type:device_code",
+          device_code: $deviceCode,
+          client_id: $clientId,
+        } satisfies Record<string, string>;
+
+        const body = new URLSearchParams(params).toString();
+
+        const res = await fetch($serverMetadata.token_endpoint, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Accept: "application/json",
+          },
+          body,
+        });
+
+        const data = await res.json();
+        setResponse(data);
+
+        if ("access_token" in data && typeof data.access_token === "string") {
+          accessToken.set(data.access_token);
+        }
+
+        if ("refresh_token" in data && typeof data.refresh_token === "string") {
+          refreshToken.set(data.refresh_token);
+        }
+      },
+    },
+    $queryClient,
+  );
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    mutation.mutate();
+  };
+
+  return (
+    <div className={cx(styles["form-wrapper"])}>
+      <Form.Root className={cx(styles.form)} onSubmit={onSubmit}>
+        <Form.Field name="endpoint">
+          <Form.Label>Token endpoint</Form.Label>
+          <Form.TextControl
+            type="url"
+            readOnly
+            value={$serverMetadata.token_endpoint}
+          />
+        </Form.Field>
+        <Form.Field name="grant-type">
+          <Form.Label>Grant type</Form.Label>
+          <Form.TextControl
+            type="text"
+            readOnly
+            value="urn:ietf:params:oauth:grant-type:device_code"
+          />
+        </Form.Field>
+        <Form.Field name="client-id">
+          <Form.Label>Client ID</Form.Label>
+          <Form.TextControl type="text" required readOnly value={$clientId} />
+          {!$clientId && (
+            <Form.ErrorMessage>
+              Client must be registered to get one
+            </Form.ErrorMessage>
+          )}
+        </Form.Field>
+        <Form.Field name="device-code">
+          <Form.Label>Code</Form.Label>
+          <Form.TextControl type="text" value={$deviceCode} readOnly />
+          {!$deviceCode && (
+            <Form.ErrorMessage>No device code available</Form.ErrorMessage>
+          )}
+        </Form.Field>
+
+        <Form.Submit size="sm" kind="secondary" disabled={!$clientId}>
+          Poll with device code
         </Form.Submit>
 
         {response && <DataViewer data={response} />}
